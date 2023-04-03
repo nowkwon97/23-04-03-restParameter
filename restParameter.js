56438f0baab3cb08d9f41f6021095b69202ff25d
leejaekwon <nowkwon97@gmail.com>
2023-04-03 12:57:35 +0900

    나머지 매개변수가 생긴 이유

--- restParameter.js
+++ restParameter.js
@@ -21,3 +21,12 @@ function restParamterExample(...properties) {
 
 console.log(restParamterExample("leejaekwon" ,"이재권", "1234", "nowkwon97@gmail.com"));
 // 매개변수에 문자열을 넣었음에도 '배열'로 처리된 것을 확인 가능
+
+// 매개변수가 너무 많아지는 경우가 필연히 발생한다.
+function tooManyParameters(a, b, c, d, e, f, g, h, i, j, k, l, n) {
+  let localArray = [a, b, c, d, e, f, g, h, i, j, k, l, n];
+  // 위 localArray와 같이 배열로 일단 넣을 수는 있겠지만
+  // 이렇게 하면, 매개변수가 늘어날 때마다, 배열도 늘려야하는 큰 문제가 발생
+  // => 그래서 나온 것이 나머지 매개변수이다.
+  return localArray;
+}
